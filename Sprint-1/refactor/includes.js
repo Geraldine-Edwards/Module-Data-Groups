a3853e0ae590df35f0fc703ae2c546bf8cb74d41
gerbil1511 <gerbil1511@gmail.com>
2025-06-20 20:43:53 +0100

    refactor includes function so that the tests still pass

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,13 +1,18 @@
 // Refactor the implementation of includes to use a for...of loop
 
+// function includes(list, target) {
+//   for (let index = 0; index < list.length; index++) {
+//     const element = list[index];
+//     if (element === target) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+
+// Because the includes() method is used to loop through arrays to check if a value exists it is doing the same check as the longer function above. The code on line 15 will simply return true if target is found in list, and false otherwise.
 function includes(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
-    if (element === target) {
-      return true;
-    }
-  }
-  return false;
+  return list.includes(target);
 }
 
 module.exports = includes;
