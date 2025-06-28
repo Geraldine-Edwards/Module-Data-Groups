5b08ddf63597544b0c9993b9f9cc27fd213c481d
gerbil1511 <gerbil1511@gmail.com>
2025-06-28 10:11:23 +0100

    update the console log statement to access the value in the property houseNumber

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,6 @@
 // Predict and explain first...
+/* I expect the code to produce an error because originally it is trying to console log the [0] index of a property in an object when there is no "0" in the address object.
+ */
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
@@ -12,4 +14,6 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address["houseNumber"]}`);
+// Or we could access the house number like this:
+console.log(`My house number is ${address.houseNumber}`);
