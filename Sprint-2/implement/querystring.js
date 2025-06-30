bcd5877bcbcee0b235cf830acc782b0d280ad21e
gerbil1511 <gerbil1511@gmail.com>
2025-06-30 18:32:21 +0100

    update parseQueryString function and add tests for edge cases

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,15 +1,24 @@
 function parseQueryString(queryString) {
   const queryParams = {};
   if (queryString.length === 0) {
+    // if the queryString is empty, we return an empty object
     return queryParams;
   }
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    //const [key, value] = pair.split("="); <- there is an issue with this as it will split ALL parts of a querystring input that has an "=" in it.
+    //however, we only want to split at the first occurrence of the "=" to create a key value pair format, therefore we can use the String.prototype indexOf() that searches the string and returns the index of the first occurrence of "="  instead and then slices the string at that index
+    const index = pair.indexOf("=");
+    if (index === -1) {
+      // if there is no "=" in the pair, we assign the pair as a key with an empty string as its value
+      queryParams[pair] = "";
+    } else {
+      const key = pair.slice(0, index); //start at index 0 and go up to the index of the first "=" and assign it to key
+      const value = pair.slice(index + 1); // assign the value to everything after the first "=" by slicing from index + 1 to the end of the string
+      queryParams[key] = value; // append the key-value pair to the queryParams object
+    }
   }
-
   return queryParams;
 }
 
--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -3,10 +3,34 @@
 // Below is one test case for an edge case the implementation doesn't handle well.
 // Fix the implementation for this test, and try to think of as many other edge cases as possible - write tests and fix those too.
 
-const parseQueryString = require("./querystring.js")
+const parseQueryString = require("./querystring.js");
 
-test("parses querystring values containing =", () => {
+test("parses empty queryString", () => {
+  expect(parseQueryString("")).toEqual({});
+});
+
+test("parses queryString values containing =", () => {
   expect(parseQueryString("equation=x=y+1")).toEqual({
-    "equation": "x=y+1",
+    equation: "x=y+1",
   });
+});
+
+test("parses queryString where value contains multiple '='", () => {
+  expect(parseQueryString("a=b=c=d")).toEqual({ a: "b=c=d" });
+});
+
+test("parses queryString with only '='", () => {
+  expect(parseQueryString("=")).toEqual({ "": "" });
+});
+
+test("parses queryString containing multiple pair values some with '=' and some without", () => {
+  expect(parseQueryString("sum=a+b&equation&total=42-y")).toEqual({
+    sum: "a+b",
+    equation: "",
+    total: "42-y",
+  });
+});
+
+test("parses queryString with values that have no '=' in the them", () => {
+  expect(parseQueryString("equation:y+1")).toEqual({ "equation:y+1": "" });
 });
