54f85450922d3fa2aa3aa1bc6d6e126099b8ae10
gerbil1511 <gerbil1511@gmail.com>
2025-06-30 20:35:20 +0100

    update tally function and add tests

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,22 @@
-function tally() {}
+function tally(arr) {
+  if (!Array.isArray(arr)) {
+    //validate that the input is in fact an array in teh first place
+    throw new TypeError("Input must be a valid array");
+  }
+  const obj = {};
+  if (arr.length === 0) {
+    //return an empty object for an empty array
+    return obj;
+  }
+  for (const item of arr) {
+    obj[item] = (obj[item] || 0) + 1; // start at 0 and check each item in the array and count the frequency of each item
+
+    /* 
+    initially the obj[item] is undefined because it is empty. A common javascript pattern is to set the assigned value to 0 (hence (... || 0)) if it is undefined (i.e. falsy), this ensures you always have a number to add "1" to.
+    In the first iteration, the value of the item in the object is checked and set to 1.  On subsequent iterations, if the value appears again, it will add 1 to the count. If a new item value is found, it will be set to 1, and so on.
+    */
+  }
+  return obj;
+}
 
 module.exports = tally;
--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -19,16 +19,46 @@ const tally = require("./tally.js");
 // Given a function called tally
 // When passed an array of items
 // Then it should return an object containing the count for each unique item
+test("given an array passed to tally function, returns an object with a count for each unique item in the array", () => {
+  const input = ["a", "b", "c"];
+  expect(tally(input)).toEqual({ a: 1, b: 1, c: 1 });
+});
 
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
-test.todo("tally on an empty array returns an empty object");
+test("given an empty array passed to tally function, returns an empty object", () => {
+  const input = [];
+  expect(tally(input)).toEqual({});
+});
 
 // Given an array with duplicate items
 // When passed to tally
 // Then it should return counts for each unique item
+describe("given an array with duplicate items", () => {
+  test.each([
+    [["a", "a", "b", "c"], { a: 2, b: 1, c: 1 }],
+    [
+      [null, undefined, false, null, true],
+      { null: 2, undefined: 1, false: 1, true: 1 },
+    ],
+    [[1, 2, 2, 3, 1, 1], { 1: 3, 2: 2, 3: 1 }],
+    [[{}, {}, []], { "[object Object]": 2, "": 1 }],
+    [["", "", "a"], { "": 2, a: 1 }],
+  ])("Given %j, it returns %j", (input, expected) => {
+    expect(tally(input)).toEqual(expected);
+  });
+});
 
 // Given an invalid input like a string
 // When passed to tally
 // Then it should throw an error
+
+describe("given an invalid input to the tally function", () => {
+  test.each(["a, b, c, a", 123, null, undefined, { a: 1, b: 2 }, true, false])(
+    "Given %j, it throws an error",
+    (arr) => {
+      expect(() => tally(arr)).toThrow("Input must be a valid array");
+    }
+  );
+});
