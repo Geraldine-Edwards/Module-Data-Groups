303309f7209fec06696befcef75d41b6b5216e2d
gerbil1511 <gerbil1511@gmail.com>
2025-07-09 20:12:37 +0100

    update script.js with function to list each book and relevant details, add a new css file to style the content for rendering in the browser

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -21,3 +21,29 @@ const books = [
   },
 ];
 
+//get the <ul> element with id reading-list from the DOM
+const readingList = document.querySelector("#reading-list");
+
+//create a list item element (for each book in the array of book objects) with a title & author text and the book cover image
+books.forEach((book) => {
+  //create the list item element for each book in the array
+  const listItem = document.createElement("li");
+
+  //set the class of the list item based on whether the book has been read or not
+  listItem.className = book.alreadyRead ? "already-read" : "not-read";
+
+  //create a text element for the book title and author
+  const bookText = document.createElement("p");
+  bookText.innerText = `${book.title} by ${book.author}`;
+
+  //create the image element for the book cover
+  const coverImg = document.createElement("img");
+  coverImg.src = book.bookCoverImage;
+
+  //append text and image to the list item
+  listItem.appendChild(bookText);
+  listItem.appendChild(coverImg);
+
+  //append the list item to the reading list
+  readingList.appendChild(listItem);
+});
